bfd9acc5f7dea7f22e02fad7d53d08ac54a5c4d7
elanetto <101054274+elanetto@users.noreply.github.com>
2025-02-21 15:34:07 +0100

    Added Layout components and started designing the Header

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,11 @@
+export function Footer () {
+    return (
+        <>
+            <footer className="footer bg-blue-950 text-white text-center">
+                <div className="container text-white">
+                    <p>This is a footer</p>
+                </div>
+            </footer>
+        </>
+    )
+}
